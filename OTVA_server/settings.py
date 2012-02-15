263664c79ca2a1740cc9f1c9c04531e5588d920b
unknown <allegfede@monster.(none)>
2012-02-15 13:52:25 +0100

    errore nella pagina di login da guardare

--- OTVA_server/settings.py
+++ OTVA_server/settings.py
@@ -11,10 +11,11 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        #'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'mysql',
+        'NAME': 'OTVAdb',                      # Or path to database file if using sqlite3.
+        'USER': 'OTVAdbuser',                      # Not used with sqlite3.
+        'PASSWORD': 'OTVAdbpassword',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
@@ -106,6 +107,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    "C:/Users/allegfede/OTVA/OTVA/OTVA_server/templates"
 )
 
 INSTALLED_APPS = (
--- OTVA_server/urls.py
+++ OTVA_server/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls.defaults import patterns, include, url
+#from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls.defaults import *
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -14,4 +15,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    (r'^login/', include('auth.views.login_user')),
 )
