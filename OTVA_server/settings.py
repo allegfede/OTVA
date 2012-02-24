f298de519c2daa9c3e8169c54c6bcda250715519
Antonello Amici <antonello.amici@gmail.com>
2012-02-24 21:51:07 +0100

    pagina di login funzionante

--- OTVA_server/settings.py
+++ OTVA_server/settings.py
@@ -99,6 +99,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+	'django.middleware.locale.LocaleMiddleware' # per abilitare il supporto alla tua lingua 
 )
 
 ROOT_URLCONF = 'OTVA_server.urls'
@@ -118,7 +119,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
--- OTVA_server/urls.py
+++ OTVA_server/urls.py
@@ -2,8 +2,8 @@
 from django.conf.urls.defaults import *
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -14,6 +14,6 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-    (r'^login/', include('auth.views.login_user')),
+    url(r'^admin/', include(admin.site.urls)),
+    # (r'^login/', include('auth.views.login_user')),
 )
