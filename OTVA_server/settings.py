977defed0274644d676498945a59ef3bb0cdbfe3
Antonello Amici <antonello.amici@gmail.com>
2012-04-13 13:15:40 +0200

    0

--- OTVA_server/settings.py
+++ OTVA_server/settings.py
@@ -134,6 +134,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 	'OTVA_server.ingesting',
+    'django_extensions'
 )
 
 # A sample logging configuration. The only tangible logging
